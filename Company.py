f99f17c979b71375f01f5d481c9f652abb459184
gsriniv1 <g@srin.me>
2015-09-06 21:13:22 -0400

    minor updates

--- Company.py
+++ Company.py
@@ -1,1 +1,6 @@
 __author__ = 'gsriniv1'
+class Company:
+    def __init__(self,nm):
+        self._name = nm
+    def __str__(self):
+        return "Company: %s" % self._name
--- load.py
+++ load.py
@@ -1,1 +1,8 @@
 __author__ = 'gsriniv1'
+import Company
+
+c1 = Company.Company('General Electric')
+c2 = Company.Company('Johnson & Johnson')
+
+print(c1)
+print(c2)
